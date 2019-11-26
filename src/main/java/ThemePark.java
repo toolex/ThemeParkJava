d5606fede2b579dcd630b93bf499d9a61aa8cdca
Iain Tooley <iaintooley1@gmail.com>
2019-11-26 17:17:00 +0000

    tred extentions

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,2 +1,19 @@
+import behaviours.IReviewed;
+import stalls.*;
+import attractions.*;
+
+import java.util.ArrayList;
+
 public class ThemePark {
+
+    private ArrayList<IReviewed> allReviewedPlaces;
+
+    public ThemePark(){
+        allReviewedPlaces = new ArrayList<IReviewed>();
+    }
+
+
+    public ArrayList<IReviewed> getAllReviewed(){
+        return
+    }
 }
